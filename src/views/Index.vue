dacfae86db2c74b52fb49940d454eba0462557de
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-07 21:51:31 +0800

    配置左侧菜单栏

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -3,6 +3,29 @@
     <!-- 侧边栏 -->
     <el-aside width="200px">
       <h3>黑马头条</h3>
+
+     
+         <!-- default-active：当前高亮选中的菜单 -->
+          <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+          
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+          >
+            <!-- index是一标识，给default-actives使用的 -->
+            <el-submenu index="1">
+                <!-- slot="title"必须要保留 -->
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>文章管理</span>
+              </template>
+              <el-menu-item index="1-1">文章列表</el-menu-item>
+              <el-menu-item index="1-2">发布文章</el-menu-item>
+            </el-submenu>
+          </el-menu>
+       
     </el-aside>
     <!-- 右侧内容部分 -->
     <el-container>
@@ -56,10 +79,14 @@ export default {
 }
 
 .el-aside {
+    width:200px;
   h3 {
-    text-align: center;
-    margin-top: 20px;
-    color: #cccccc;
+    height: 60px;
+      width:200px;
+      line-height: 60px;
+      text-align: center;
+      font-size:18px;
+      color:#eee;
   }
   background-color: #000000;
   color: #333;
