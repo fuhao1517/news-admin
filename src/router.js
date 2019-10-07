fce2ce6d51c57aa7e4c2145a3208fba0e5dfa681
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-07 18:48:49 +0800

    项目的基本结构

--- src/router.js
+++ src/router.js
@@ -1,25 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
+/* 注册路由插件 */
 Vue.use(Router)
-
+/*创建路由对象 */
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
+  /* 路由的配置 */
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+
   ]
 })
