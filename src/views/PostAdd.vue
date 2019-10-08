7e55d5cc65cefbd36413b8759656e02c620f5fd5
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-08 20:02:49 +0800

    调用富文本编辑器

--- src/views/PostAdd.vue
+++ src/views/PostAdd.vue
@@ -1,30 +1,78 @@
 <template>
-  <div style="width:80%;margin-top:30px;">
-    <el-form ref="form" :model="form" label-width="80px">
+  <div style="padding:30px">
+    <el-form :model="form" label-width="80px">
       <el-form-item label="标题">
-        <el-input v-model="form.name"></el-input>
+        <el-input v-model="form.title"></el-input>
       </el-form-item>
 
-      <el-form-item label="内容">
-        <el-input type="textarea" :rows="2" placeholder="请输入内容" v-model="form.content"></el-input>
-      </el-form-item>
+      <div>
+        <el-form-item label="内容">
+          <VueEditor :config="config" style="background:#fff;"/>
+        </el-form-item>
+      </div>
     </el-form>
   </div>
 </template>
 
 <script>
+import VueEditor from "vue-word-editor";
+// 需要单独引入样式
+import "quill/dist/quill.snow.css";
 export default {
+  name: "app",
   data() {
     return {
       /* 表单的数据对象 */
-      form: {
-        title: "",
-        content: "",
-        categories: [],
-        cover: [],
-        type: 1
+      form: {},
+      config: {
+        modules: {
+          // 工具栏
+          toolbar: [
+            ["bold", "italic", "underline", "strike"], // toggled buttons
+            ["blockquote", "code-block"],
+            ["image", "video"],
+
+            [{ header: 1 }, { header: 2 }], // custom button values
+            [{ list: "ordered" }, { list: "bullet" }],
+            [{ script: "sub" }, { script: "super" }], // superscript/subscript
+            [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+            [{ direction: "rtl" }] // text direction
+          ]
+        },
+        // 主题
+        theme: "snow",
+        uploadImage: {
+          url: "http://localhost:3000/upload",
+          name: "files",
+          uploadBefore(file) {
+            return true;
+          },
+          uploadProgress(res) {},
+          uploadSuccess(res, insert) {
+            insert("http://localhost:3000" + res.data[0].url);
+          },
+          uploadError() {},
+          showProgress: false
+        },
+
+        uploadVideo: {
+          url: "http://localhost:1337/upload",
+          name: "files",
+          uploadBefore(file) {
+            return true;
+          },
+          uploadProgress(res) {},
+          uploadSuccess(res, insert) {
+            insert("http://localhost:1337" + res.data[0].url);
+          },
+          uploadError() {}
+        }
       }
     };
+  },
+
+  components: {
+    VueEditor
   }
 };
 </script>
