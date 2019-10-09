cb082dc95cd1adb5b33127e55830e3b99f57f955
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-09 18:52:53 +0800

    退出登录

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -36,7 +36,7 @@
       <el-header>
         <img :src="$axios.defaults.baseURL+user.user.head_img" alt />
         <span>{{user.user.nickname}}</span>
-        <i>退出</i>
+        <i @click="handleLogout" style="cursor:pointer">退出</i>
       </el-header>
       <!-- 子页面显示的内容 -->
       <el-main>
@@ -68,7 +68,18 @@ export default {
       return arr.join("/");
     }
   },
-  mounted() {}
+  methods: {
+    handleLogout() {
+      this.$confirm("确定退出?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(() => {
+        localStorage.removeItem("user");
+        this.$router.replace("/login");
+      });
+    }
+  }
 };
 </script>
 
--- src/views/PostEdit.vue
+++ src/views/PostEdit.vue
@@ -37,7 +37,7 @@
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button type="primary" @click="onSubmit">保存</el-button>
       </el-form-item>
     </el-form>
   </div>
